71253dc04064969d476b9550ba11872ce6b30ae5
weiyafei <weiyafei@flashwhale.com>
2023-11-17 18:34:55 +0800

    feat: 通知暂存

--- src/pages/setting.tsx
+++ src/pages/setting.tsx
@@ -1,7 +1,42 @@
-import React from "react";
+import { Button, Switch, message } from "antd";
 
 const Setting = () => {
-  return <div>Setting</div>;
+  const compatible = function () {
+    if (typeof Notification === "undefined") {
+      message.info("浏览器暂不支持消息通知");
+      return false;
+    }
+    return true;
+  };
+
+  const handleOnChange = (value: any) => {
+    if (value && compatible()) {
+      Notification.requestPermission(function (permission) {
+        console.log(permission);
+      });
+    }
+  };
+
+  return (
+    <div>
+      <Switch title="开启通知" onChange={handleOnChange} />
+      <Button
+        onClick={() => {
+          if (compatible()) {
+            new Notification("会议开始通知", {
+              body: "您预约的会议即将开始，请注意及时入会哦！",
+              tag: "0",
+              icon: "",
+              renotify: true,
+              dir: "auto",
+            });
+          }
+        }}
+      >
+        测试通知
+      </Button>
+    </div>
+  );
 };
 
 export default Setting;
